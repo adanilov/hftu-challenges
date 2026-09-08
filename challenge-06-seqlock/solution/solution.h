8966f52ca7bb8f7c23ef1138e15780e4b779ca3c
adanilov <alex.daniels.au@gmail.com>
2026-09-09 03:19:40 +0700

    experiment with alignas fields

--- challenge-06-seqlock/solution/solution.h
+++ challenge-06-seqlock/solution/solution.h
@@ -30,7 +30,7 @@ namespace hftu {
 
     class Seqlock {
         alignas(64) std::atomic<uint32_t> seq_{0};
-        Payload data_{};
+        alignas(64) Payload data_{};
     public:
         Seqlock() = default;
 
